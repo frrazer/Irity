0f7a005ca335d53d157203943de3d860e96d8dd5
noxirity <frazersearle07@gmail.com>
2024-06-17 14:27:41 +0200

    begin rbx market monitor

--- services/databaseService.js
+++ services/databaseService.js
@@ -1,8 +1,21 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, Db } = require("mongodb");
 
+/**
+ * MongoDB connection URI.
+ * @type {string}
+ */
 const uri = process.env.MONGO_URI;
+
+/**
+ * MongoDB client instance.
+ * @type {MongoClient}
+ */
 let client;
 
+/**
+ * Connects to MongoDB and returns the client instance.
+ * @returns {Promise<MongoClient>} The MongoDB client.
+ */
 const connectToMongoDB = async () => {
   if (!client) {
     client = new MongoClient(uri);
@@ -11,6 +24,11 @@ const connectToMongoDB = async () => {
   return client;
 };
 
+/**
+ * Retrieves a MongoDB database instance.
+ * @param {string} databaseName The name of the database to connect to.
+ * @returns {Promise<Db>} The database instance.
+ */
 const getDatabase = async (databaseName) => {
   try {
     const client = await connectToMongoDB();
