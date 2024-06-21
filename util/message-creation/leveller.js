3eb5836b01673165c27b285a5f7dad4fb5911f77
noxirity <frazersearle@outlook.com>
2024-06-21 21:19:25 +0200

    Add transaction recorder

---
+++ util/message-creation/leveller.js
@@ -0,0 +1,109 @@
+const calculateLevel = require("../calculateLevel");
+const databaseService = require("../../services/databaseService");
+
+async function level(message) {
+    const authorId = message.author.id;
+    const CHANNEL_ID = "1057661334185054338";
+
+    const levelRoles = [
+        { level: 5, role: "1249484824406659214" },
+        { level: 10, role: "1249484815195967559" },
+        { level: 25, role: "1210321267257909339" },
+        { level: 50, role: "1210321263763923005" },
+        { level: 75, role: "1210321260765253652" },
+        { level: 100, role: "1210321251453636638" },
+    ];
+
+    try {
+        const database = await databaseService.getDatabase("DiscordServer");
+        const collection = database.collection("CasinoEmpireLevelling");
+        let document = await collection.findOne({ user_id: authorId });
+
+        if (!document) {
+            document = {
+                user_id: authorId,
+                tracking: {
+                    messages: 1,
+                    xp: 0,
+                    last_message: Date.now(),
+                },
+                settings: {
+                    level_up_notification: true,
+                },
+                cacheing: {
+                    last_updated: Date.now(),
+                    username: message.author.username,
+                }
+            };
+            await collection.insertOne(document);
+        }
+
+        const { tracking } = document;
+        const { messages, xp, last_message } = tracking;
+        let xpToAdd = Math.floor(15 + Math.random() * 11);
+
+        const xp_multiplier_expire = 1718578800
+        const xp_multiplier = 2.5
+
+        if (Date.now() / 1000 > xp_multiplier_expire) {
+            xpToAdd *= xp_multiplier;
+        }
+
+        const newXp = xp + xpToAdd;
+        const newMessages = messages + 1;
+
+        if (last_message + 60000 > Date.now()) return;
+        await collection.updateOne({ user_id: authorId }, {
+            $set: {
+                "tracking.messages": newMessages,
+                "tracking.xp": newXp,
+                "tracking.last_message": Date.now(),
+                "cacheing.last_updated": Date.now(),
+                "cacheing.username": message.author.username,
+            }
+        });
+
+        const currentLevel = calculateLevel(xp).currentLevel;
+        const newLevel = calculateLevel(newXp).currentLevel;
+
+        if (newLevel > currentLevel) {
+            const notifyChannel = await client.channels.fetch(CHANNEL_ID);
+            let messageContent = `Congratulations, {{user}}! You have officially reached level **${newLevel}**! 🎉`;
+
+            const member = await notifyChannel.guild.members.fetch(authorId);
+            let highestRole = null;
+            let roleUpdated = false;
+
+            for (const levelRole of levelRoles) {
+                if (newLevel >= levelRole.level) {
+                    highestRole = levelRole;
+                }
+            }
+
+            if (highestRole) {
+                const rolesToRemove = levelRoles.filter(role => role.level < highestRole.level && member.roles.cache.has(role.role));
+                for (const role of rolesToRemove) {
+                    await member.roles.remove(role.role);
+                    roleUpdated = true;
+                }
+
+                if (!member.roles.cache.has(highestRole.role)) {
+                    await member.roles.add(highestRole.role);
+                    roleUpdated = true;
+                }
+            }
+
+            if (roleUpdated && document.settings.level_up_notification || newLevel === 1) {
+                messageContent += "\n\n> If you do not wish to be pinged for level ups, you can disable this notification by using the **/settings level-up-notification** command.";
+            }
+
+            messageContent = document.settings.level_up_notification ? messageContent.replace("{{user}}", `<@${authorId}>`) : messageContent.replace("{{user}}", member.user.username);
+
+            notifyChannel.send({ content: messageContent });
+        }
+    } catch (error) {
+        console.error('Error handling regular message:', error);
+    }
+}
+
+module.exports = level;
---
+++ util/message-creation/transactionMonitor.js
@@ -0,0 +1,88 @@
+const databaseService = require("../../services/databaseService");
+const crypto = require('crypto');
+
+async function transactionMonitor(message) {
+    const allowed_channels = ["1112734635210846310", "1142738352731332689"];
+    if (!allowed_channels.includes(message.channel.id)) return;
+    if (!message.author.bot) return;
+
+    try {
+        switch (message.channel.id) {
+            case "1112734635210846310":
+                await handleRAPChangesChannel(message);
+                break;
+            case "1142738352731332689":
+                await handleTipsChannel(message);
+                break;
+            default:
+                console.log("Unhandled channel ID:", message.channel.id);
+        }
+    } catch (error) {
+        console.error("Failed to handle message:", error);
+    }
+}
+
+async function handleRAPChangesChannel(message) {
+    const embed = message.embeds[0];
+    if (!embed) return;
+
+    const [item_name, serial] = embed.title.replace(/^✨/, '').trim().split("#").map(s => s.trim());
+    const { fields, footer } = embed;
+    const buyer_id = fields[3]?.value.match(/https:\/\/www\.roblox\.com\/users\/(\d+)\/profile/)?.[1] ?? null;
+    const seller_id = fields[4]?.value.match(/https:\/\/www\.roblox\.com\/users\/(\d+)\/profile/)?.[1] ?? null;
+    const sale_price = parseInt(fields[2]?.value.replace(/,/g, ""), 10);
+    const sale_date = new Date(message.createdTimestamp);
+    const transaction_id = footer?.text.split(": ")[1] ?? null;
+
+    const database = await databaseService.getDatabase("ArcadeHaven");
+    const collection = database.collection("items");
+    const item = await collection.findOne({ name: item_name }, { projection: { itemId: 1 } });
+
+    if (!item) {
+        console.error("Failed to find item:", item_name);
+        return;
+    }
+
+    const transaction = {
+        item_id: `${item.itemId}`,
+        serial,
+        buyer_id,
+        seller_id,
+        sale_price,
+        sale_date,
+        transaction_id,
+        type: "marketplace"
+    };
+
+    await saveTransaction(transaction);
+}
+
+async function handleTipsChannel(message) {
+    const [tipper, tipped, rawAmount] = [...message.content.match(/https:\/\/www\.roblox\.com\/users\/\$?(\d+)\/profile/g).map(url => url.match(/\d+/)[0]), message.content.split(" ").pop()];
+    const amount = parseInt(rawAmount.replace(/[$,]/g, ""), 10) || 0;
+    const tip_date = new Date(message.createdTimestamp);
+    const generateTransactionId = () => crypto.randomUUID().replace(/-/g, '').split('').map(() => Math.random() < 0.5 ? String.fromCharCode(Math.floor(Math.random() * 10) + 48) : String.fromCharCode(Math.floor(Math.random() * 26) + 65)).join('').slice(0, 8);
+
+    const transaction = {
+        tipper,
+        tipped,
+        amount,
+        tip_date,
+        transaction_id: generateTransactionId(),
+        type: "tip"
+    };
+
+    await saveTransaction(transaction);
+}
+
+async function saveTransaction(transaction) {
+    try {
+        const database = await databaseService.getDatabase("DiscordServer");
+        const collection = database.collection("game_transactions");
+        await collection.insertOne(transaction);
+    } catch (error) {
+        console.error("Failed to save transaction:", error);
+    }
+}
+
+module.exports = transactionMonitor;
