c910e2a92182b9c2fb05e06e65007bfb5cc0029f
noxirity <frazersearle@outlook.com>
2024-06-26 00:17:01 +0200

    autodrop changes

--- util/message-creation/autodropFiller.js
+++ util/message-creation/autodropFiller.js
@@ -1,5 +1,6 @@
 const { stringToDuration } = require("../functions")
 const { getDatabase } = require("../../services/databaseService")
+const axios = require("axios");
 
 let tracking_ids = []
 
@@ -13,6 +14,27 @@ async function toggleTracking(user_id, channel_id) {
         return true;
     }
 }
+
+async function extract_details() {
+    const html = (await axios.get("https://www.rolimons.com/itemtable")).data;
+    const startString = "<script>var item_details = ";
+    const endString = ";</script>";
+    const startIndex = html.indexOf(startString);
+    if (startIndex === -1) {
+        return null;
+    }
+    const endIndex = html.indexOf(endString, startIndex);
+    if (endIndex === -1) {
+        return null;
+    }
+    const jsonString = html.substring(startIndex + startString.length, endIndex);
+    const details = JSON.parse(jsonString);
+
+    return details;
+}
+
+let details = null;
+
 async function execute(message, client) {
     const index = tracking_ids.findIndex(item => item.user_id === message.author.id && item.channel_id === message.channel.id);
     if (index === -1) return;
@@ -35,21 +57,19 @@ async function execute(message, client) {
     }
 
     if (Number(quantity)) {
-        if (Number(quantity) <= 10) {
-            return message.reply("You don't have clearance to drop items with a quantity of 10 or less. Please double-check and try again.");
+        if (Number(quantity) <= 25) {
+            return message.reply("You don't have clearance to drop items with a quantity of 25 or less. Please double-check and try again.");
         } else if (Number(quantity) > 1000) {
             return message.reply("You don't have clearance to drop items with a quantity of 1,000 or more. Please double-check and try again.");
         }
 
-        if (Number(quantity) <= 50) {
-            // we need to check the ratio of how many other documents have less than 50
-            // if the ratio is too high, we need to reject the request to prevent too many rare items
+        if (Number(quantity) <= 60) {
             const auto_dropper = (await getDatabase("ArcadeHaven")).collection("auto_dropper");
             const count = await auto_dropper.countDocuments({ quantity: { $lte: 50 }, dropped: { $ne: true } });
             const total = await auto_dropper.countDocuments({ dropped: { $ne: true } });
             const ratio = count / total;
 
-            if (ratio > 0.08) { // 1 rare every ~12 items
+            if (ratio > 0.07) {
                 return message.reply("There are too many rare items in the autodropper. Please try adding this again later.");
             }
         }
@@ -87,6 +107,26 @@ async function execute(message, client) {
 
     auto_dropper.insertOne(doc);
     message.react("✅");
+
+
+    // Add data to neural network training database
+    const training_data = (await getDatabase("ArcadeHaven")).collection("autodrop_training_data");
+    if (!details) {
+        details = await extract_details();
+    }
+
+    const item_info = details[item_id];
+    const real_quantity = item_info[11];
+    const real_price = item_info[8];
+    training_data.insertOne({
+        item_id: Number(item_id),
+        real_price: real_price,
+        real_quantity: real_quantity,
+        est_price: Number(price),
+        est_quantity_or_date: is_duration ? stringToDuration(quantity) : Number(quantity),
+        user: message.author.id,
+        date: new Date()
+    })
 }
 
 module.exports = {
