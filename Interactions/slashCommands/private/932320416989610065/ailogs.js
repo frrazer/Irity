1cf5c5f2a152130fa27f8ff63c4e74411e4b35eb
noxirity <frazersearle07@gmail.com>
2024-07-13 01:22:05 +0200

    Allow external messages

--- Interactions/slashCommands/private/932320416989610065/ailogs.js
+++ Interactions/slashCommands/private/932320416989610065/ailogs.js
@@ -1,35 +1,62 @@
 const {
-  SlashCommandBuilder,
-  EmbedBuilder,
-  SlashCommandSubcommandBuilder,
-} = require("discord.js");
-const { chatgpt } = require("../../../../util/runAi");
+    SlashCommandBuilder,
+    EmbedBuilder,
+    SlashCommandSubcommandBuilder,
+  } = require("discord.js");
+  const { chatgpt } = require("../../../../util/runAi");
+  const databaseService = require("../../../../services/databaseService");
+  
+  const settings = {
+    list: new SlashCommandSubcommandBuilder()
+      .setName("list")
+      .setDescription("Get the 15 most recent thread IDs."),
+    thread: new SlashCommandSubcommandBuilder()
+      .setName("thread")
+      .setDescription("Get a thread's data.")
+      .addStringOption((option) =>
+        option
+          .setName("thread_id")
+          .setDescription("The thread ID to lookup.")
+          .setRequired(true)
+      ),
+  };
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("ailogs")
+      .setDescription("Get AI logs.")
+      .addSubcommand(settings.list)
+      .addSubcommand(settings.thread),
+    roles: ["1213276024020934666"],
+    async execute(interaction, client) {
+      const subcommand = interaction.options.getSubcommand();
+  
+      if (subcommand === "list") {
+        const database = await databaseService.getDatabase("DiscordServer");
+        const collection = database.collection("ValueAIThreads");
+        const threads = await collection.find().limit(15).toArray();
+        const threadIds = threads.map((thread) => thread.thread_id);
+        const threadIdsString = threadIds.join("\n");
+  
+        return interaction.reply({
+          content: threadIdsString,
+        });
+      } else if (subcommand === "thread") {
+        await interaction.deferReply();
 
-const settings = {
-  list: new SlashCommandSubcommandBuilder()
-    .setName("list")
-    .setDescription("Get the 15 most recent thread IDs."),
-  thread: new SlashCommandSubcommandBuilder()
-    .setName("thread")
-    .setDescription("Get a thread's data.")
-    .addStringOption((option) =>
-      option
-        .setName("thread_id")
-        .setDescription("The thread ID to lookup.")
-        .setRequired(true)
-    ),
-};
+        const threadId = interaction.options.getString("thread_id");
+        const thread = (await chatgpt.beta.threads.messages.list(threadId)).data;
+        let str = "";
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ailogs")
-    .setDescription("Get AI logs."),
-  roles: ["1213276024020934666"],
-  async execute(interaction, client) {
-    const subcommand = interaction.options.getSubcommand();
+        thread.reverse();
+        thread.forEach((message) => {
+          str += `**${message.role}:** ${message.content[0].text.value}\n\n`;
+        });
 
-    if (subcommand === "list") {
-      chatgpt.beta.threads.runs.
-    }
-  },
-};
+        return interaction.editReply({
+          content: str,
+        });
+      }
+    },
+  };
+  
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,34 +1,79 @@
 const { cooldown, permissions } = require("../util/functions");
 const { PREFIX: prefix } = require("../util/config.json");
+const redis = require("redis");
 
-const checkForLinks = require("../util/message-creation/checkForLinks")
-const level = require("../util/message-creation/leveller")
-const autodropFiller = require("../util/message-creation/autodropFiller")
-const transactionMonitor = require("../util/message-creation/transactions")
+const checkForLinks = require("../util/message-creation/checkForLinks");
+const level = require("../util/message-creation/leveller");
+const autodropFiller = require("../util/message-creation/autodropFiller");
+const transactionMonitor = require("../util/message-creation/transactions");
+
+const { promisify } = require("util");
+const { createClient } = require("redis");
+const setTimeoutPromise = promisify(setTimeout);
+
+let isConnected = false;
+const store = {};
+const redisClient = redis.createClient();
+const subscriber = redisClient.duplicate();
+
+async function setupSubscriber() {
+  if (!isConnected) {
+    await subscriber.connect();
+    isConnected = true;
+
+    await subscriber.subscribe("irity-message-create", (message) => {
+      const data = JSON.parse(message);
+      const message_id = data.message_id;
+      if (store[message_id]) return;
+
+      store[message_id] = {
+        content: data.content,
+        embeds: data.embeds,
+        attachments: data.attachments,
+        components: data.components,
+      };
+    });
+  }
+}
+
+async function getMessageFromStore(messageId) {
+  const startTime = Date.now();
+  const timeout = 5000; // 5 seconds
+
+  while (Date.now() - startTime < timeout) {
+    if (store[messageId]) return store[messageId];
+    await setTimeoutPromise(100); // check every 100ms
+  }
+
+  return null;
+}
 
 module.exports = {
   name: "messageCreate",
   once: false,
   async execute(message, client) {
-    if (message.from_server) {
-      const channel = await client.channels.fetch(message.channelId);
-      const real_message = await channel.messages.fetch(message.id);
-      if (!real_message) return;
-
-      real_message.content = message.content;
-      real_message.embeds = message.embeds;
-      real_message.attachments = message.attachments;
-      real_message.components = message.components;
-
-      message = real_message;
+    if (message.guild.id === "932320416989610065") {
+      await setupSubscriber();
+      const messageData = await getMessageFromStore(message.id);
+      if (messageData) {
+        message.content = messageData.content;
+        message.embeds = messageData.embeds;
+        message.attachments = messageData.attachments;
+        message.components = messageData.components;
+      }
     }
 
     let commandPrefix = prefix;
-    const mentionRegex = new RegExp(`^<@!?(${client.user.id})>`, "gi").exec(message.content);
+    const mentionRegex = new RegExp(`^<@!?(${client.user.id})>`, "gi").exec(
+      message.content
+    );
     if (mentionRegex) commandPrefix = `${mentionRegex[0]} `;
 
     if (message.content.startsWith(commandPrefix)) {
-      const args = message.content.slice(commandPrefix.length).trim().split(/ +/);
+      const args = message.content
+        .slice(commandPrefix.length)
+        .trim()
+        .split(/ +/);
       const commandName = args.shift().toLowerCase();
       const command = findCommand(client, commandName);
       if (!command) return;
@@ -71,7 +116,7 @@ function isOnCooldown(message, command, client) {
   return cooldown(message, command, message.author.id, client);
 }
 
-const WHITELISTED_GUILDS = ["932320416989610065"]
+const WHITELISTED_GUILDS = ["932320416989610065"];
 
 async function handleRegularMessage(message, client) {
   if (!WHITELISTED_GUILDS.includes(message.guild.id)) return;
